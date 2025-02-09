2bc2061796bfb1f2b8f05e7a00c49312f5878c3c
Pedro Rocha <pedrosr711@gmail.com>
2025-02-09 10:59:46 +0100

    workaround dds issues

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,8 +10,11 @@ ros2 run antsy_control follow_velocity_rectangle &
 echo "Launching robot description..."
 ros2 launch antsy_description description.launch.py &
 
-# echo "Enabling motors..."
-# ros2 run hiwonder_ros2 write_motors_only &
+echo "Allowing remote control..."
+ros2 launch ds4_launcher joy_teleop.launch.py &
+
+echo "Enabling motors..."
+ros2 run hiwonder_ros2 write_only &
 
 # source install/setup.bash
 # exec bash

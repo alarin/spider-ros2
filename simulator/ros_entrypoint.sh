6c985bd67e3d1ccb231e03a171ccc6b389e3a91e
Tony Larin <tonylarin@Tonys-Mac-mini.local>
2025-10-05 12:55:36 +0300

    working simulator on mac

--- simulator/ros_entrypoint.sh
+++ simulator/ros_entrypoint.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 echo "ANTSY Simulator Container"
+
+#bash /entrypoint.sh &
+
+source /opt/ros/jazzy/setup.sh
 source install/setup.bash
-ros2 launch antsy_simulation gazebo.launch.py
+#exec bash
+#ros2 launch antsy_simulation gz.launch.py
+ros2 launch antsy_simulation simulator.launch.xml
+
 exec "$@"
